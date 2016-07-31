6c7036e55e9266481ee55d0d8b562f56ff46f1fb
Scott Weston <scott@srw.id.au>
2016-07-31 10:49:45 +1000

    updated by Isaac to send events only when state change is detected

--- sensor/parko.ino
+++ sensor/parko.ino
@@ -1,14 +1,13 @@
 int lightS = 0;
-unsigned long lasttime = 0UL;
-
+int carold = 0;
+int car = 0;
 void setup() {
     pinMode(D7, OUTPUT);
+     digitalWrite(D7, HIGH);
     pinMode(A5, INPUT);
-
-		// calibarte the sensor to current lighting conditions
     lightS = analogRead(A5);
     delay(1000);
-    lightS = analogRead(A5) + lightS;
+     digitalWrite(D7, HIGH);
     delay(1000);
     lightS = analogRead(A5) + lightS;
     delay(1000);
@@ -16,35 +15,53 @@ void setup() {
     delay(1000);
     lightS = analogRead(A5) + lightS;
     lightS = lightS / 5 - 300;
+     digitalWrite(D7, LOW);
 }
 
 void loop()
 {
-    unsigned long now = millis();
     int lightL = analogRead(A5);
-
-    Serial.printf("light = %d/%d\n\r", lightL, lightS);
-
-    if (now-lasttime>60000UL)
-    {
-        lasttime = now;
-
-        if (lightL < lightS)
-        {
-            Spark.publish("occupied", "True");
-        }
-        else
-        {
-            Spark.publish("occupied", "False");
-        }
-    }
-
     if (lightL < lightS)
     {
-        digitalWrite(D7, HIGH);
+       car = 1;
     }
     else
     {
+       car = 2;
+    }
+    //car change- 1 = there is a car. 2 = no car.
+
+    Serial.printf("light = %d/%d\n\r", lightL, lightS);
+
+    if (car != carold)
+    {
+        digitalWrite(D7, HIGH);
+        delay(1500);
+        int lightL = analogRead(A5);
+        if (lightL < lightS)
+        {
+           car = 1;
+        }
+        else
+        {
+           car = 2;
+        }
+        if(car != carold)
+        {
+            carold = car;
+
+            if (lightL < lightS)
+            {
+                Spark.publish("occupied", "True");
+                digitalWrite(D7, LOW);
+            }
+            else
+            {
+                Spark.publish("occupied", "False");
+                digitalWrite(D7, LOW);
+            }
+        }
         digitalWrite(D7, LOW);
     }
+    delay(1000);
 }
